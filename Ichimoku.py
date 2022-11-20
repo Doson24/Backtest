9bba3ec197c2344c00c08d8167bbaab6af79b8a9
Doson <karbushev-1991@mail.ru>
2022-11-20 13:26:46 +0700

    create

--- Ichimoku.py
+++ Ichimoku.py
@@ -1,13 +1,69 @@
-from TAcharts.indicators.ichimoku import Ichimoku
 from backtesting.test import GOOG
+from backtesting.lib import crossover
+from backtesting import Strategy
+import pandas_ta as ta
+from backtesting import Backtest
+
+
+def spanA(data):
+    # Data OHLCV
+    ichimoku = ta.ichimoku(data.High.s, data.Low.s, data.Close.s)
+    print(ichimoku[0].to_numpy().T)
+    return ichimoku[0].to_numpy().T[0]
+
+
+def spanB(data):
+    # Data OHLCV
+    ichimoku = ta.ichimoku(data.High.s, data.Low.s, data.Close.s)
+    return ichimoku[0].to_numpy().T[1]
+
+
+def tenkan_Sen(data):
+    # Data OHLCV
+    ichimoku = ta.ichimoku(data.High.s, data.Low.s, data.Close.s)
+    return ichimoku[0].to_numpy().T[2]
+
+
+def kijun_Sen(data):
+    # Data OHLCV
+    ichimoku = ta.ichimoku(data.High.s, data.Low.s, data.Close.s)
+    return ichimoku[0].to_numpy().T[3]
+
+
+def chikou_Span(data):
+    # Data OHLCV
+    ichimoku = ta.ichimoku(data.High.s, data.Low.s, data.Close.s)
+    return ichimoku[0].to_numpy().T[4]
+
+
+class Ichimoku_cross(Strategy):
+    # n1 = 20
+    # n2 = 25
+
+    def init(self):
+        self.span_A = self.I(spanA, self.data)
+        self.span_B = self.I(spanB, self.data)
+        self.tenkan_sen = self.I(tenkan_Sen, self.data)
+        self.kijun_sen = self.I(kijun_Sen, self.data)
+        self.chikou_span = self.I(chikou_Span, self.data)
+
+
+    def next(self):
+        if self.position:
+            if self.data.Close[-1] < self.kijun_sen[-1] and \
+                    self.tenkan_sen[-1] < self.kijun_sen[-1]:
+                self.position.close()
+
+        else:
+            if self.data.Close[-1] > self.span_A[-1] > self.span_B[-1] and \
+                    self.tenkan_sen[-1] > self.kijun_sen[-1] and \
+                    self.chikou_span[-1] > self.data.Close[-26]:
+                self.buy()
 
 
 if __name__ == '__main__':
-    df = GOOG[:1000]
-    df['Date'] = df.index
-    df.reset_index(drop=True, inplace=True)
-
-    i = Ichimoku(df)
-    i.build(20, 60, 120, 30)
-    i.plot()
-
+    bt = Backtest(GOOG, Ichimoku_cross, cash=10000, commission=.002,
+                  exclusive_orders=True)
+    stats = bt.run()
+    bt.plot()
+    print(stats)
